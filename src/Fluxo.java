9b632551e1182e56c42553bc4f2fec7e684875b7
montenegrohenrique <montenegrohenrique@192.168.1.101>
2019-07-04 00:18:32 -0300

    Aula 02 -  Tratamento de excecoes

--- src/Fluxo.java
+++ src/Fluxo.java
@@ -2,7 +2,16 @@ public class Fluxo {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+        	metodo1();
+        } catch (ArithmeticException | NullPointerException e) {
+			System.out.println("Excecao: " + e.getMessage());
+         	 e.printStackTrace();
+        }
+//		} catch (NullPointerException e) {
+//			System.out.println("Excecao: " + e.getMessage());
+//			e.printStackTrace();
+//		}
         System.out.println("Fim do main");
     }
 
@@ -16,6 +25,10 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
+            
+			int a = i / 0;
+			Conta c = null;
+			c.deposita();
         }
         System.out.println("Fim do metodo2");
     }
